9b56745effdff1ed4ead6e2f3fc1ed575621c7e0
Gillian Laming <glaming@microsoft.com>
2022-04-07 08:12:47 -0700

    working for private stamps (need to adjust C:\home vs D:\home accordingly

--- StressTestFunction/Function1.cs
+++ StressTestFunction/Function1.cs
@@ -2,28 +2,26 @@ using System;
 using System.IO;
 using System.Diagnostics;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
-using Microsoft.Extensions.Logging;
 
 namespace StressTestFunction
 {
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer)
         {
-			log.LogInformation("starting the stress test");
+			//log.LogInformation("starting the stress test");
 			Guid instanceGuid = Guid.NewGuid();
-			string rootDir = @"D:\home\site\wwwroot";
+			string rootDir = @"C:\home\site\wwwroot";
 			string pathToReadFile = Path.Combine(rootDir, instanceGuid.ToString(), "TestDirectory0", "TestFile0.bin");
 			string folderPath = Path.Combine(rootDir, instanceGuid.ToString());
 			Directory.CreateDirectory(folderPath);
 
-			string applicationFilePath = @"D:\home\site\wwwroot\IOTests.exe";
-			log.LogInformation($"Path to write files is {folderPath}");
-			log.LogInformation($"application path is {applicationFilePath}");
+			string applicationFilePath = @"C:\home\site\wwwroot\IOTests.exe";
+			//log.LogInformation($"Path to write files is {folderPath}");
+			//log.LogInformation($"application path is {applicationFilePath}");
 
-			Process.Start(applicationFilePath, $"writeperftest -p {folderPath} -c 3000 -cs 1000 -t 5 -cc 1000 -s 1 -v 1 -dc 1 -wt 1 -fc 1");
+			Process.Start(applicationFilePath, $"writeperftest -p {folderPath} -c 3000 -cs 1000 -t 5 -cc 1024 -s 1 -v 1 -dc 1 -wt 1 -fc 1");
 			Process.Start(applicationFilePath, $"testfileopenclose {pathToReadFile} 100000");
 		}
 	}
